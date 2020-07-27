b10a254a56268fd57055d086e0e3db34d0f700b0
GhozeylM <g.alkhalifa@tuwaiq.edu.sa>
2020-07-28 00:06:33 +0300

    enhacement:[or.js] module has added and used inside app.js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const and = require('./and');
 let result = and([1, 0, 0, 1], [1, 1, 1, 0]);
 console.log(result);
+//or 
+const or = require('./or');
+let resultOr = or([1, 0, 0, 1], [1, 1, 1, 0]);
+console.log(resultOr);
