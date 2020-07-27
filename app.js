8e2e47316edf1a0abfe268de0d766663800f6499
GhozeylM <g.alkhalifa@tuwaiq.edu.sa>
2020-07-28 00:09:13 +0300

    enhacement:[not.js] module has added and used inside app.js

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ console.log(result);
 const or = require('./or');
 let resultOr = or([1, 0, 0, 1], [1, 1, 1, 0]);
 console.log(resultOr);
+
+//not 
+const not = require('./not');
+let resultNot = not([1, 0, 0, 1]);
+console.log(resultNot);
